b59581596fd8b9743cd660755c3dbaa115978d2f
pythonai250824 <pythonai250824@gmail.com>
2024-11-24 18:56:11 +0200

    upload

--- time_data.py
+++ time_data.py
@@ -9,3 +9,13 @@ print(datetime.now().strftime("%H:%M:%S.%f %Y-%B-%d %A"))
 print( (datetime.now() + timedelta(days=1)) .strftime("%H:%M:%S.%f %Y-%B-%d %A"))
 print( (datetime.now() + timedelta(days=30)) .strftime("%H:%M:%S.%f %Y-%B-%d %A"))
 print( (datetime.now() + timedelta(hours=90)) .strftime("%H:%M:%S.%f %Y-%B-%d %A"))
+
+# create a new list
+# input names from the user until 'done'
+# append each name to a list with a timestamp as tuple
+# danny
+#    0                        ,  1
+# [ (datetime.now(), 'danny') , ( )]
+# print all the time+name and index
+# for key, value ...
+#  0: danny 18:55  1: sharon 18:56
